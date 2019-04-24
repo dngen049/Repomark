8780c21649047efd83627b15a7acf64dc24dd320
Diedrick Ng <dngen049@uottawa.ca>
2019-04-23 22:16:51 -0400

    Added add and removing functionnality

--- src/App.js
+++ src/App.js
@@ -5,11 +5,75 @@ import Rowd from "./Component/Rowd"
 
 
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            input: "",
+            data: [],
+            favor : []
+        }
+        this.onRemoving = this.onRemoving.bind(this)
+        this.onAdding = this.onAdding.bind(this)
+    }
     
+      onRemoving(index){
+       
+            const arrayCopy = this.state.favor.filter((row) => row.key !== index);
+            const element = this.state.data.find((row) => row.key === index)
+             var i =  this.state.data.indexOf(element);
+            const arrayCopy1 = this.state.data.filter((row) => row.key !== index);
+            
+             var isAdd = false
+             if(element.isAdded === false){
+                isAdd = true
+            }
+            const newelm = {
+                key:element.key,
+                name: element.name,
+                Owner: element.Owner,
+                lang:element.lang,
+                Url: element.Url,
+                isAdded: isAdd
+            }
+           
+            arrayCopy1.splice(i, 0, newelm)
+            this.setState({data: arrayCopy1,favor: arrayCopy});
+         
+        }
+     onAdding(index){
+        const element = this.state.data.find((row) => row.key === index)
+        
+        var i =  this.state.data.indexOf(element);
+        const arrayCopy = this.state.data.filter((row) => row.key !== index)
+        const arrayCopy1 = this.state.favor.slice();
+
+       
+         var isAdd = false
+        if(element.isAdded === false){
+            isAdd = true
+        }
+         const newelm = {
+              key:element.key,
+              name: element.name,
+              Owner: element.Owner,
+              lang:element.lang,
+              Url: element.Url,
+              isAdded: isAdd
+         }
+        
+         arrayCopy.splice(i, 0, newelm)
+         arrayCopy1.push(element)
+          this.setState({data: arrayCopy,
+          favor:arrayCopy1});
+       
+     
+     }
+     
+     
     render() {
         return (
             <div>
-                
+               
                 <div  className="TabContainer">
                     <table className="ResultTab"> 
                         <tbody>
